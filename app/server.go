85032aa5486e26b591f6f0e35ed6f2509478e153
Aman Tuladhar <atuladhar.aman@gmail.com>
2024-09-22 12:38:55 -0500

    Nnot found

--- app/server.go
+++ app/server.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"log/slog"
 	"net"
 	"os"
+	"strings"
 
 	"github.com/codecrafters-io/http-server-starter-go/pkg/util"
 )
@@ -13,6 +15,7 @@ var _ = net.Listen
 var _ = os.Exit
 
 func main() {
+	slog.SetLogLoggerLevel(slog.LevelDebug)
 	fmt.Println("Logs from your program will appear here!")
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	util.ExitOnErr(err, "Failed to bind to port 4221")
@@ -20,5 +23,30 @@ func main() {
 	conn, err := l.Accept()
 	util.ExitOnErr(err, "Error accepting connection")
 	defer conn.Close()
-	conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+
+	// parse request
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+	util.LogOnErr(err, "unable to read from connection")
+
+	lines := strings.Split(string(buf[:n]), "\r\n")
+	_, path, _, err := parseStatusLine(lines[0])
+	util.LogOnErr(err, "unable to parse status line")
+
+	response := ""
+	if path == "/" {
+		response = "HTTP/1.1 200 OK\r\n\r\n"
+	} else {
+		response = "HTTP/1.1 404 Not Found\r\n\r\n"
+	}
+	//response
+	conn.Write([]byte(response))
+}
+
+func parseStatusLine(line string) (method string, path string, version string, err error) {
+	parts := strings.Split(line, " ")
+	if len(parts) != 3 {
+		return "", "", "", fmt.Errorf("invalid status line")
+	}
+	return parts[0], parts[1], parts[2], nil
 }
--- pkg/util/log.go
+++ pkg/util/log.go
@@ -7,15 +7,62 @@ import (
 	"runtime"
 )
 
+func LogInfo(msg string, args ...interface{}) {
+	_, f, l, ok := runtime.Caller(1)
+	file := "unknown"
+	line := 0
+	if ok {
+		file = f
+		line = l
+	}
+	slog.Info(msg, append(args, "file", fmt.Sprintf(" %s:%d", file, line))...)
+}
+
+func LogDebug(msg string, args ...interface{}) {
+	_, f, l, ok := runtime.Caller(1)
+	file := "unknown"
+	line := 0
+	if ok {
+		file = f
+		line = l
+	}
+	slog.Debug(msg, append(args, "file", fmt.Sprintf(" %s:%d", file, line))...)
+}
+
+func LogErr(msg string, args ...interface{}) {
+	_, f, l, ok := runtime.Caller(1)
+	file := "unknown"
+	line := 0
+	if ok {
+		file = f
+		line = l
+	}
+	slog.Error(msg, append(args, "file", fmt.Sprintf(" %s:%d", file, line))...)
+}
+
+func LogOnErr(err error, msg string, args ...interface{}) {
+	if err == nil {
+		return
+	}
+	_, f, l, ok := runtime.Caller(1)
+	file, line := "unknown", 0
+	if ok {
+		file = f
+		line = l
+	}
+	slog.Error(msg, append(args, "err", err, "file", fmt.Sprintf(" %s:%d", file, line))...)
+}
+
 func ExitOnErr(err error, msg string, args ...interface{}) {
 	if err == nil {
 		return
 	}
 	_, f, l, ok := runtime.Caller(1)
+	file, line := "unknown", 0
 	if ok {
-		slog.Error(msg, append(args, "err", err, "file", fmt.Sprintf(" %s:%d", f, l))...)
-	} else {
-		slog.Error(msg, append(args, "err", err)...)
+		file = f
+		line = l
 	}
+	slog.Error(msg, append(args, "err", err, "file", fmt.Sprintf(" %s:%d", file, line))...)
 	os.Exit(1)
 }
